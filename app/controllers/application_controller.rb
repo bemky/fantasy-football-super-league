404f164037e3c200b7697aaa6e9dd4c4c9786375
Ben Ehmke <benehmke@gmail.com>
2019-09-06 15:42:47 -0500

    manager name mapping

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,9 +45,7 @@ class ApplicationController < ActionController::Base
           league_id: league['league_id'],
           league: league['name'],
           points: team['team_points']['total'].to_i,
-          manager: team['managers']['manager'].kind_of?(Array) ? team['managers']['manager'].map do |manager|
-            manager.try(:[], 'nickname')
-          end.join(", ") : team['managers']['manager']['nickname']
+          manager: manager(team, league)
         }
       end
       @week_number = league['scoreboard']['week']
@@ -64,6 +62,75 @@ class ApplicationController < ActionController::Base
   def raw
     index
     render json: @data
+  end
+  
+  def manager(team, league)
+    mapping = {
+      '1101180.1': 'Patrick',
+      '808962.11': 'Kunal',
+      '808962.10': 'Ryan',
+      '808962.9': 'Tanner',
+      '808962.8': 'Paul',
+      '808962.7': 'Brent S',
+      '808962.6': 'Maxwell',
+      '808962.5': 'David',
+      '808962.4': 'William',
+      '808962.3': 'kareem',
+      '808962.2': 'Hillel',
+      '808962.1': 'Raymond',
+      '1057144.12': 'John',
+      '1057144.11': 'Miyagi',
+      '1057144.10': 'Sergey',
+      '1057144.9': 'Andrew',
+      '1057144.8': 'Not Paying, ravi',
+      '1057144.7': '-- hidden --',
+      '1057144.6': 'Chris',
+      '1057144.5': 'Justin',
+      '1057144.4': 'Anthony',
+      '1057144.3': 'Angela',
+      '1057144.2': 'Taylor',
+      '1057144.1': 'Ryan',
+      '829718.12': 'Chris',
+      '829718.11': 'Jim',
+      '829718.10': 'Nathan',
+      '829718.9': 'Stephen',
+      '829718.8': 'Jerry Won',
+      '829718.7': 'Roshni, Kerianne',
+      '829718.6': 'kevin',
+      '829718.5': 'Ray',
+      '829718.4': 'Ben K',
+      '829718.3': 'Riley',
+      '829718.2': 'Paul Duca',
+      '829718.1': 'Chris',
+      '1101180.12': 'david',
+      '1101180.11': 'Eug Lee',
+      '1101180.10': 'Vik',
+      '1101180.9': 'AllanT',
+      '1101180.8': 'David',
+      '1101180.7': 'Ian',
+      '1101180.6': 'Ayla',
+      '1101180.5': 'Ben',
+      '1101180.4': 'Erin',
+      '1101180.3': 'Zak Levy',
+      '1101180.2': 'Charles',
+      '808962.12': 'David'
+    }
+    if team['managers']['manager'].kind_of?(Array)
+      names = team['managers']['manager'].map do |manager|
+        manager.try(:[], 'nickname')
+      end
+      names -= ["Knotel"]
+      puts "*"*80
+      puts names.inspect, names.include?("--hidden--")
+      puts "*"*80
+      
+      return mapping["#{league["league_id"]}.#{team["team_id"]}".to_sym] || '--hidden--' if names.include?("--hidden--")
+      names.join(", ")
+    else
+      name = team['managers']['manager']['nickname']
+      return mapping["#{league["league_id"]}.#{team["team_id"]}".to_sym] || '--hidden--' if name == "--hidden--"
+      name
+    end
   end
   
 end
